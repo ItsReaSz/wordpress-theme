ef348313c3a86cb3f454e03005f11da950cbedf8
Sander <swiekamp@gmail.om>
2018-11-02 13:11:12 +0100

    wordpress changes

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The Header for our theme.
  *
@@ -11,10 +12,10 @@
 <html <?php language_attributes(); ?>>
 <head>
 <?php thinkup_hook_header(); ?>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta charset="<?php bloginfo('charset'); ?>" />
 <meta name="viewport" content="width=device-width" />
 <link rel="profile" href="//gmpg.org/xfn/11" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/lib/scripts/html5.js" type="text/javascript"></script>
 <![endif]-->
@@ -28,7 +29,7 @@
 
 	<header id="site-header">
 
-		<?php if ( get_header_image() ) : ?>
+		<?php if (get_header_image()) : ?>
 			<div class="custom-header"><img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt=""></div>
 		<?php endif; // End header image check. ?>
 
@@ -36,8 +37,8 @@
 		<div class="wrap-safari">
 		<div id="pre-header-core" class="main-navigation">
   
-			<?php if ( has_nav_menu( 'pre_header_menu' ) ) : ?>
-			<?php wp_nav_menu( array( 'container_class' => 'header-links', 'container_id' => 'pre-header-links-inner', 'theme_location' => 'pre_header_menu' ) ); ?>
+			<?php if (has_nav_menu('pre_header_menu')) : ?>
+			<?php wp_nav_menu(array('container_class' => 'header-links', 'container_id' => 'pre-header-links-inner', 'theme_location' => 'pre_header_menu')); ?>
 			<?php endif; ?>
 
 			<?php /* Header Search */ thinkup_input_headersearch(); ?>
@@ -53,12 +54,12 @@
 		<div id="header-core">
 
 			<div id="logo">
-			<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php /* Custom Logo */ thinkup_custom_logo(); ?></a>
+			<a rel="home" href="<?php echo esc_url(home_url('/')); ?>"><?php /* Custom Logo */ thinkup_custom_logo(); ?></a>
 			</div>
 
 			<div id="header-links" class="main-navigation">
 			<div id="header-links-inner" class="header-links">
-				<?php wp_nav_menu(array('container' => false, 'theme_location'  => 'header_menu' ) ); ?>
+				<?php wp_nav_menu(array('container' => false, 'theme_location' => 'header_menu')); ?>
 			</div>
 			</div>
 			<!-- #header-links .main-navigation -->
@@ -75,7 +76,6 @@
 	<?php /*  Call To Action - Intro */ thinkup_input_ctaintro(); ?>
 	<?php /*  Pre-Designed HomePage Content */ thinkup_input_homepagesection(); ?>
 
-	<div id="content">
 	<div id="content-core">
 
 		<div id="main">
